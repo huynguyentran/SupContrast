aaee9385a2afe259e662d2ddc062d2d0bdbf6f3f
Huy Nguyen <77764356+huynguyentran@users.noreply.github.com>
2025-02-23 14:41:25 -0800

    Update resnet_big.py

--- networks/resnet_big.py
+++ networks/resnet_big.py
@@ -145,6 +145,7 @@ model_dict = {
     'resnet50': [resnet50, 2048],
     'resnet101': [resnet101, 2048],
     # 'vit_small': [models.vision_transformer.vit_s_16, 384],
+    'vit_s_dino: [torch.hub.load('facebookresearch/dinov2', 'dinov2_vits14_lc'), 384],
     'vit_base': [models.vision_transformer.vit_b_16, 768], 
     'vit_large': [models.vision_transformer.vit_l_16, 1024] 
 }
