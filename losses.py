43765e434fe8265153b43cf9dba1593f10bb145b
Huy Nguyen <77764356+huynguyentran@users.noreply.github.com>
2025-03-06 17:53:36 -0800

    Update losses.py

--- losses.py
+++ losses.py
@@ -92,7 +92,10 @@ class SupConLoss(nn.Module):
         # compute log_prob
         exp_logits = torch.exp(logits) * logits_mask
         
-        log_prob = logits - torch.log(exp_logits.sum(1, keepdim=True))
+        # log_prob = logits - torch.log(exp_logits.sum(1, keepdim=True))
+
+        log_prob = logits - torch.log(torch.clamp(exp_logits.sum(1, keepdim=True), min=1e-9))
+        
         # denominator = exp_logits.sum(1, keepdim=True)
         # denominator = torch.where(denominator == 0, torch.tensor(1.0, device=denominator.device), denominator)
         # log_prob = logits - torch.log(denominator)
